32e46e9337882d3a87d6bacd438255e6f739c696
cyhaz <cyhaz@naver.com>
2020-03-16 14:21:30 +0900

    01 시작

---
+++ app/src/main/java/kr/co/tjoeun/helloworld/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.co.tjoeun.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+//    프로젝트 첫 주석
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
